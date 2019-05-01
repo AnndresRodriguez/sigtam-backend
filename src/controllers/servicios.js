5f791611135be1b62409c5f78ed25139ab722a6f
Andres Joel Carrillo Rodriguez <andresjoe24@gmail.com>
2019-05-01 09:40:14 -0500

    Inicializando endpoints servicios

--- src/controllers/servicios.js
+++ src/controllers/servicios.js
@@ -7,68 +7,68 @@ router.get("/", async (req, res) => {
     res.json(servicios);
   });
   
-  router.post("/addmany", async (req, res, next)=>{
-    Servicio.insertMany([
+  // router.post("/addmany", async (req, res, next)=>{
+  //   Servicio.insertMany([
   
-        {
-            "id": 1,
-            "nombre": "Reparación"
-          },
-          {
-            "id": 2,
-            "nombre": "Mantenimiento"
-          },
-          {
-            "id": 3,
-            "nombre": "Revisión Preventiva"
-          },
-          {
-            "id": 4,
-            "nombre": "Revisión Tecnico Mecánica"
-          },
-          {
-            "id": 5,
-            "nombre": "Alineación y Balanceo"
-          },
-          {
-            "id": 6,
-            "nombre": "Cambio de Aceite"
-          },
-          {
-            "id": 7,
-            "nombre": "Servicio de Scanner"
-          },
-          {
-            "id": 8,
-            "nombre": "Sincronización Eléctrica"
-          },
-          {
-            "id": 9,
-            "nombre": "Sincronización Full Inyeccion"
-          },
-          {
-            "id": 10,
-            "nombre": "Caliración de Gases"
-          },
-          {
-            "id": 11,
-            "nombre": "Bajada de Caja"
-          },
-          {
-            "id": 12,
-            "nombre": "Bajada de Tanque"
-          },
-          {
-            "id": 13,
-            "nombre": "Cambio de Pastillas de Frenos"
-          },
-          {
-            "id": 14,
-            "nombre": "Codificación de llave"
-          }    
+  //       {
+  //           "id": 1,
+  //           "nombre": "Reparación"
+  //         },
+  //         {
+  //           "id": 2,
+  //           "nombre": "Mantenimiento"
+  //         },
+  //         {
+  //           "id": 3,
+  //           "nombre": "Revisión Preventiva"
+  //         },
+  //         {
+  //           "id": 4,
+  //           "nombre": "Revisión Tecnico Mecánica"
+  //         },
+  //         {
+  //           "id": 5,
+  //           "nombre": "Alineación y Balanceo"
+  //         },
+  //         {
+  //           "id": 6,
+  //           "nombre": "Cambio de Aceite"
+  //         },
+  //         {
+  //           "id": 7,
+  //           "nombre": "Servicio de Scanner"
+  //         },
+  //         {
+  //           "id": 8,
+  //           "nombre": "Sincronización Eléctrica"
+  //         },
+  //         {
+  //           "id": 9,
+  //           "nombre": "Sincronización Full Inyeccion"
+  //         },
+  //         {
+  //           "id": 10,
+  //           "nombre": "Caliración de Gases"
+  //         },
+  //         {
+  //           "id": 11,
+  //           "nombre": "Bajada de Caja"
+  //         },
+  //         {
+  //           "id": 12,
+  //           "nombre": "Bajada de Tanque"
+  //         },
+  //         {
+  //           "id": 13,
+  //           "nombre": "Cambio de Pastillas de Frenos"
+  //         },
+  //         {
+  //           "id": 14,
+  //           "nombre": "Codificación de llave"
+  //         }    
         
-    ])
-    res.json({archived:true})
-  })
+  //   ])
+  //   res.json({archived:true})
+  // })
 
 module.exports = router
