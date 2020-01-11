534cc11b68bf16e1db8105db1ba06ac7d45dabdd
Andres Joel Carrillo Rodriguez <andresjoe24@gmail.com>
2020-01-11 11:39:07 -0500

    Actualizacion Mongoose

--- src/server.js
+++ src/server.js
@@ -22,33 +22,34 @@ const productosController = require("./controllers/productos");
 const proveedoresController = require("./controllers/proveedores");
 const serviciosController = require("./controllers/servicios");
 const vehiculosController = require("./controllers/vehiculos");
+require('dotenv').config()
 
 //------------------------------Multer-----------------------------------------
 
 const storage = multer.diskStorage({
 
-	destination: path.join(__dirname, 'files/uploads/'),
-	filename: (req, file, cb) =>{
-		cb(null, uuid() + path.extname(file.originalname).toLowerCase());
-	}
+    destination: path.join(__dirname, 'files/uploads/'),
+    filename: (req, file, cb) => {
+        cb(null, uuid() + path.extname(file.originalname).toLowerCase());
+    }
 
 });
 
 const upload = app.use(multer({
-	storage,
-	dest: path.join(__dirname, 'files/uploads/'),
-	limits: {filesize: 3000000},
-	fileFilter: (req, file, cb) => {
-		const fileTypes = /pdf|doc|docx/;
-		const mimetype = fileTypes.test(file.mimetype);
-		const extname = fileTypes.test(path.extname(file.originalname));
+    storage,
+    dest: path.join(__dirname, 'files/uploads/'),
+    limits: { filesize: 3000000 },
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /pdf|doc|docx/;
+        const mimetype = fileTypes.test(file.mimetype);
+        const extname = fileTypes.test(path.extname(file.originalname));
 
-		if(mimetype && extname){
-			return cb(null, true)
-		}
+        if (mimetype && extname) {
+            return cb(null, true)
+        }
 
-		cb("Error: los archivos deben ser pdf, doc, docx")
-	}
+        cb("Error: los archivos deben ser pdf, doc, docx")
+    }
 }).single('file'))
 
 //--------------------------------------------------------------------------------
@@ -58,17 +59,28 @@ const upload = app.use(multer({
 app.use(cors());
 
 //Bodyparser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 //Settings Port
 app.set("port", process.env.PORT || 3000);
 
-//Database
-mongoose.connect("mongodb://localhost/sigtam" , {useNewUrlParser: true}) 
-.then(db => console.log('Connection established'))
-.catch(err => console.log(err));
+//Database Local
+// mongoose.connect("mongodb://localhost/sigtam", { useNewUrlParser: true })
+//     .then(db => console.log('Connection established'))
+//     .catch(err => console.log(err));
+
+//Database Production
+const MongoClient = require('mongodb').MongoClient;
+const uri = `${ process.env.PASSWORD_DB }`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("sigtam")
+        // perform actions on the collection object
+    client.close();
+});
+
 //Connection Private
 // mongoose.connect('mongodb://AndresRodriguez:root@localhost/mecanicapp' , {useNewUrlParser: true})
 
@@ -99,5 +111,5 @@ app.use('/vehiculos', vehiculosController);
 
 //Listening server
 app.listen(app.get("port"), () => {
-  console.log("Server on port", app.get("port"));
+    console.log("Server on port", app.get("port"));
 });
